f085cf43d12a69058c593312968f9c5d17b0f232
onkore <john.titor.1977@gmail.com>
2017-03-20 17:32:18 +0900

    wrap App with MuiThemeProvider

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 import AvailableNGWords from '../containers/AvailableNGWords'
 import ClearNGWords from '../containers/ClearNGWords'
@@ -6,10 +7,10 @@ import ClearNGWords from '../containers/ClearNGWords'
 export default class App extends React.Component {
   render() {
     return (
-      <div>
+      <MuiThemeProvider>
         <AvailableNGWords />
         <ClearNGWords />
-      </div>
+      </MuiThemeProvider>
     )
   }
 }
