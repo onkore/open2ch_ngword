679b724824515fe85e467750f9f336a7f67521fe
onkore <john.titor.1977@gmail.com>
2017-03-21 11:03:43 +0900

    inject tap event plugin

--- src/entries/popup.js
+++ src/entries/popup.js
@@ -2,7 +2,10 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { Store } from 'react-chrome-redux'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 import App from '../components/App'
+
+injectTapEventPlugin()
 
 const store = new Store({portName: 'OPEN2CH_NGWORD'})
 
